7441fefc775408479840ac5e074ef9df0be6289c
Grayson Wall <gwall@valdosta.edu>
2023-08-29 00:52:15 -0400

    COMMITING PERSONALLY

--- ACME-System/src/ver1/HelloWorld.java
+++ ACME-System/src/ver1/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello from Eclipse");
+		System.out.println("Ashe is working locally on her branch");
 	}
 
 }
