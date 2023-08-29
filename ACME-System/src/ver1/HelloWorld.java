e437ce30c535508bf50c8a2f4a70c7c5bed7d875
Grayson Wall <gwall@valdosta.edu>
2023-08-29 01:48:02 -0400

    Whatfor added

--- ACME-System/src/ver1/HelloWorld.java
+++ ACME-System/src/ver1/HelloWorld.java
@@ -7,6 +7,12 @@ public class HelloWorld {
 		System.out.println(Msg);
 	}
 	
+	public static void whatFor(String x) {
+		String Msg = ", what for?";
+		System.out.println(x+Msg);
+	}
+
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ashe is working locally on her branch");
